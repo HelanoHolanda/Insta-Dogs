c7599166d7b576004f3c102461b46b9a0b6573b6
HelanoHolanda <holandahelano02@gmail.com>
2024-04-13 18:54:23 -0300

    teste

--- src/Containers/User/UserHeaderNav.jsx
+++ src/Containers/User/UserHeaderNav.jsx
@@ -3,9 +3,7 @@ import React, { useEffect, useState } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import { useUser } from "../../hooks/UserContext";
 import UseMedia from "../../hooks/UseMedia";
-import adicionar from "../../public/adicionar.svg";
-import feed from "../../Public/feed.svg";
-import sair from "../../Public/sair.svg";
+
 const UserHeaderNav = () => {
   const { logout } = useUser();
 
@@ -73,7 +71,7 @@ const UserHeaderNav = () => {
               : "bg-slate-200 rounded-md h-10 w-10 flex items-center justify-center border border-transparent border-solid duration-100 cursor-pointer focus:bg-white focus:shadow-md  focus:shadow-gray-400 focus:border-zinc-800 focus:outline-none hover:bg-white hover:shadow-md hover:shadow-gray-4 hover:border-zinc-800 hover:outline-none  active:bg-white  active:shadow active:shadow-yellow-200  active:border-yellow-500"
           } `}
         >
-          <img className="inline-block" src={feed} alt="Feed" />
+          <img className="inline-block" src="/adicionar.svg" alt="Feed" />
           {mobile && "Minhas Fotos"}
         </NavLink>
 
@@ -85,7 +83,7 @@ const UserHeaderNav = () => {
               : "bg-slate-200 rounded-md h-10 w-10 flex items-center justify-center border border-transparent border-solid duration-100 cursor-pointer focus:bg-white focus:shadow-md  focus:shadow-gray-400 focus:border-zinc-800 focus:outline-none hover:bg-white hover:shadow-md hover:shadow-gray-4 hover:border-zinc-800 hover:outline-none  active:bg-white  active:shadow active:shadow-yellow-200  active:border-yellow-500"
           } `}
         >
-          <img className="inline-block" src={adicionar} alt="Feed" />
+          <img className="inline-block" src="/adicionar.svg" alt="Feed" />
           {mobile && "Postar Foto"}
         </NavLink>
 
@@ -98,7 +96,11 @@ const UserHeaderNav = () => {
               : "bg-slate-200 rounded-md h-10 w-10 flex items-center justify-center border border-transparent border-solid duration-100 cursor-pointer focus:bg-white focus:shadow-md  focus:shadow-gray-400 focus:border-zinc-800 focus:outline-none hover:bg-white hover:shadow-md hover:shadow-gray-4 hover:border-zinc-800 hover:outline-none  active:bg-white  active:shadow active:shadow-yellow-200  active:border-yellow-500"
           } `}
         >
-          <img className="inline-block" src={sair} alt="Feed" />{" "}
+          <img
+            className="inline-block"
+            src="./Public/adicionar.svg"
+            alt="Feed"
+          />{" "}
           <button> {mobile && "Sair"}</button>{" "}
         </NavLink>
       </nav>
